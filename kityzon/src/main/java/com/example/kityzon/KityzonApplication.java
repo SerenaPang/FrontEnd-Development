20200f26ca19bd62a8fe40b85dedbf0a3dcefa01
SerenaPang4 <ypang4@usfca.edu>
2025-02-12 15:19:42 -0800

    added kityzon project

---
+++ kityzon/src/main/java/com/example/kityzon/KityzonApplication.java
@@ -0,0 +1,13 @@
+package com.example.kityzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KityzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KityzonApplication.class, args);
+	}
+
+}
---
+++ kityzon/src/test/java/com/example/kityzon/KityzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kityzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KityzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
