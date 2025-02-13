2e91f221d0a99b0891b296656eb76be7c2f9b2f8
SerenaPang4 <ypang4@usfca.edu>
2025-02-13 15:54:37 -0800

    added product model

---
+++ kityzon/src/main/java/com/example/kityzon/model/Product.java
@@ -0,0 +1,37 @@
+package com.example.kityzon.model;
+
+public class Product {
+	private int id;
+	private String name;
+	private String brand;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", brand=" + brand + ", price=" + price + "]";
+	}
+
+}
